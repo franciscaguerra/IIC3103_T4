55083e1c6ec25f41df6975bed8963ecb430a2f27
Francisca Guerra <franciscaguerra@MacBook-Pro-de-Francisca.local>
2021-06-14 11:08:24 -0400

    Fix prevalence of

--- chile.py
+++ chile.py
@@ -5,7 +5,7 @@ import gspread
 from gspread_dataframe import set_with_dataframe
 
 
-metrics = ['Number of deaths', 'Number of infant deaths', 'Number of under-five deaths', 'Mortality rate for 5-14 year-olds (probability of dying per 1000 children aged 5-14 years)', 'Adult mortality rate (probability of dying between 15 and 60 years per 1000 population)','Estimates of number of homicides', 'Crude suicide rates (per 100 000 population)', 'Mortality rate attributed to unintentional poisoning (per 100 000 population)', 'Number of deaths attributed to non-communicable diseases, by type of disease and sex', 'Estimated road traffic death rate (per 100 000 population)', 'Estimated number of road traffic deaths', 'Mean BMI (kg/m&#xb2;) (crude estimate)', 'Mean BMI (kg/m&#xb2;) (age-standardized estimate)', 'Prevalence of obesity among adults, BMI > 30 (age-standardized estimate) (%)', 'Prevalence of obesity among children and adolescents, BMI > +2 standard deviations above the median (crude estimate) (%)', 'Prevalence of overweight among adults, BMI &amp;GreaterEqual; 25 (age-standardized estimate) (%)', 'Prevalence of overweight among children and adolescents, BMI > +1 standard deviations above the median (crude estimate) (%)', 'Prevalence of underweight among adults, BMI < 18.5 (age-standardized estimate) (%)', 'Prevalence of thinness among children and adolescents, BMI &lt; -2 standard deviations below the median (crude estimate) (%)', 'Alcohol, recorded per capita (15+) consumption (in litres of pure alcohol)', 'Estimate of daily cigarette smoking prevalence (%)', 'Estimate of daily tobacco smoking prevalence (%)', 'Estimate of current cigarette smoking prevalence (%)', 'Estimate of current tobacco smoking prevalence (%)', 'Mean systolic blood pressure (crude estimate)', 'Mean fasting blood glucose (mmol/l) (crude estimate)', 'Mean Total Cholesterol (crude estimate)']
+metrics = ['Number of deaths', 'Number of infant deaths', 'Number of under-five deaths', 'Mortality rate for 5-14 year-olds (probability of dying per 1000 children aged 5-14 years)', 'Adult mortality rate (probability of dying between 15 and 60 years per 1000 population)','Estimates of number of homicides', 'Crude suicide rates (per 100 000 population)', 'Mortality rate attributed to unintentional poisoning (per 100 000 population)', 'Number of deaths attributed to non-communicable diseases, by type of disease and sex', 'Estimated road traffic death rate (per 100 000 population)', 'Estimated number of road traffic deaths', 'Mean BMI (kg/m&#xb2;) (crude estimate)', 'Mean BMI (kg/m&#xb2;) (age-standardized estimate)', 'Prevalence of obesity among adults, BMI > 30 (age-standardized estimate) (%)', 'Prevalence of obesity among children and adolescents, BMI > +2 standard deviations above the median (crude estimate) (%)', 'Prevalence of overweight among adults, BMI &amp;GreaterEqual; 25 (age-standardized estimate) (%)', 'Prevalence of overweight among children and adolescents, BMI > +1 standard deviations above the median (crude estimate) (%)', 'Prevalence of underweight among adults, BMI < 18.5 (age-standardized estimate) (%)', 'Prevalence of thinness among children and adolescents, BMI &lt; -2 standard deviations below the median (crude estimate) (%)', 'Alcohol, recorded per capita (15+) consumption (in litres of pure alcohol)', 'Estimate of daily cigarette smoking prevalence (%)', 'Estimate of daily tobacco smoking prevalence (%)', 'Estimate of current cigarette smoking prevalence (%)', 'Estimate of current tobacco smoking prevalence (%)', 'Mean systolic blood pressure (crude estimate)', 'Mean fasting blood glucose (mmol/l) (crude estimate)', 'Mean Total Cholesterol (crude estimate)','Prevalence of overweight among adults, BMI &GreaterEqual; 25 (age-standardized estimate) (%)','Prevalence of obesity among adults, BMI &GreaterEqual; 30 (age-standardized estimate) (%)', 'Prevalence of thinness among children and adolescents, BMI < -2 ']
 dict_data = {
     'COUNTRY': [],
     'YEAR': [],
@@ -70,8 +70,8 @@ def read_root(root):
 
 
 if __name__ == '__main__':
-    request_uno = requests.get('http://tarea-4.2021-1.tallerdeintegracion.cl/gho_CHL.xml').text
-    print("Datos de Chile procesados")
+    request_uno = requests.get('http://tarea-4.2021-1.tallerdeintegracion.cl/gho_CMR.xml').text
+    print("Datos de Camerun procesados")
     root_uno = ET.XML(request_uno)
     read_root(root_uno)
     request_dos = requests.get('http://tarea-4.2021-1.tallerdeintegracion.cl/gho_ESP.xml').text
@@ -101,6 +101,7 @@ if __name__ == '__main__':
     gc = gspread.service_account(filename='taller-tarea-4-316707-7653144657a3.json')
     sh = gc.open_by_key('19Vej7hkNSJBw5J0sX8kqpmdz-koeQopyFwpv8LBnrGo')
     worksheet = sh.get_worksheet(0)
+    worksheet.clear()
     set_with_dataframe(worksheet, df)
         
 #chile_df = pd.DataFrame(data=chile_json)
